83f16f1fa70267bc3711fe98d0266301607005af
mohd <ymohd022@gmail.com>
2024-10-08 12:03:09 +0530

    .

--- SD-HUB/src/app/app.module.ts
+++ SD-HUB/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -15,12 +25,20 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
   ],
-  providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- SD-HUB/src/app/registration/registration.component.ts
+++ SD-HUB/src/app/registration/registration.component.ts
@@ -1,35 +1,68 @@
-import {Component, inject} from '@angular/core';
-import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
-/**
- * @title Stepper overview
- */
 @Component({
-  selector: 'stepper-overview-example',
-  templateUrl: 'stepper-overview-example.html',
-  styleUrl: 'stepper-overview-example.css',
-  standalone: true,
-  imports: [
-    MatButtonModule,
-    MatStepperModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-  ],
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
 })
-export class StepperOverviewExample {
-  private _formBuilder = inject(FormBuilder);
+export class RegistrationComponent implements OnInit {
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  logoPath = 'assets/images/sd-hub-logo.jpg';
 
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  isLinear = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    this.firstFormGroup = this.formBuilder.group({
+      studentId: ['', Validators.required],
+      applicationDate: ['', Validators.required],
+      firstName: ['', Validators.required],
+      middleName: [''],
+      lastName: ['', Validators.required],
+      fatherFirstName: ['', Validators.required],
+      fatherMiddleName: [''],
+      fatherLastName: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      guardianContact: ['', Validators.required],
+      householdIncome: ['', Validators.required]
+    });
+
+    this.secondFormGroup = this.formBuilder.group({
+      courseApplied: ['', Validators.required],
+      degree: ['', Validators.required],
+      collegeName: ['', Validators.required],
+      yearOfPassing: ['', Validators.required],
+      percentage: ['', [Validators.required, Validators.min(0), Validators.max(100)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.firstFormGroup.valid && this.secondFormGroup.valid) {
+      const formData = {
+        ...this.firstFormGroup.value,
+        ...this.secondFormGroup.value
+      };
+      console.log('Registration form submitted', formData);
+      this.snackBar.open('Registration successful!', 'Close', { duration: 3000 });
+      this.router.navigate(['/login']);
+    } else {
+      this.snackBar.open('Please fill all required fields', 'Close', { duration: 3000 });
+    }
+  }
+
+  reset(): void {
+    this.firstFormGroup.reset();
+    this.secondFormGroup.reset();
+  }
 }
